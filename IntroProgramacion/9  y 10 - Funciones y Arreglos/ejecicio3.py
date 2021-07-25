c7fe8ad661ff51823e0f2bbc164a7ea29e1b3688
4d4143 <manuelalejandrocamino@gmail.com>
2021-07-25 17:36:34 -0300

    Resuelto ejercicio 3

--- IntroProgramacion/9  y 10 - Funciones y Arreglos/ejecicio3.py
+++ IntroProgramacion/9  y 10 - Funciones y Arreglos/ejecicio3.py
@@ -15,6 +15,48 @@ def generadorListaPosNeg():
 
     return tempArray
 
+def promedioArray(array):
+    tempArray = array
+    sumaElementos = 0
+    for elemento in tempArray:
+        sumaElementos += elemento
+    promedioElementos = sumaElementos / len(array)
+
+    return promedioElementos
+
+def generadorMultiplosCuatro(arrayFuente):
+    arrayTemp = []
+    for elemento in arrayFuente:
+        if elemento % 4 == 0:
+            arrayTemp.append(elemento)
+
+    return arrayTemp
+
+def mostrarMultiplosTres(arrayFuente):
+    tempArray = []
+    for elemento in arrayFuente:
+        if elemento % 3 == 0:
+            tempArray.append(elemento)
+
+    if tempArray:
+        print(tempArray)
+    else:
+        print("No hay multiplos de 3 entre los multiplos de 4!")
+
 if __name__ == '__main__':
     arrayEjercicio = generadorListaPosNeg()
     print(arrayEjercicio)
+
+    arrayNegativos = arrayEjercicio[5:]
+    print(promedioArray(arrayNegativos))
+
+    arrayEjercicio.sort()
+    print(arrayEjercicio)
+
+    arrayMultiplosCuatro = generadorMultiplosCuatro(arrayEjercicio)
+    if arrayMultiplosCuatro:
+        print(arrayMultiplosCuatro)
+        mostrarMultiplosTres(arrayMultiplosCuatro)
+    else:
+        print("No hay multiplos de cuatro entre los numeros introducidos!")
+ 
